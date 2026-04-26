78dd0572d2cde532d175e2b9fb97524f41fa0e95
Brian Mbawa <munashebrian19@gmail.com>
2026-04-26 02:01:26 +0200

    fix: remove unused useState to satisfy Vercel CI build

--- src/components/basket.js
+++ src/components/basket.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { CartContext } from '../contexts/cartContext';
 import { Link, useNavigate } from 'react-router-dom';
